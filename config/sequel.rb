2b1f7b774e457954be88d3c90987e4459934f380
Gilles Mathurin <gillesmath@me.com>
2014-05-23 15:40:05 -0400

    removed Logger declaraton in database connection

--- config/sequel.rb
+++ config/sequel.rb
@@ -1,4 +1,4 @@
 require 'sequel'
 
 # Connecting to database
-DB = Sequel.connect('mysql2://531485_eptt:p3W7bg6S@174.143.28.26/531485_eptt', max_connections: 4, logger: Logger.new('log/db.log'))
+DB = Sequel.connect('mysql2://531485_eptt:p3W7bg6S@174.143.28.26/531485_eptt', max_connections: 4)
