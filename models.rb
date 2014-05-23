ed2f0afe3a7098b42eedd01f537ed199d9ff16b4
Gilles Mathurin <gillesmath@me.com>
2014-05-23 18:52:06 -0400

    specified models associations

--- models.rb
+++ models.rb
@@ -5,41 +5,63 @@ require_relative 'config/sequel'
 require "active_support/core_ext/date_time/calculations"
 
 class Users < Sequel::Model
+  one_to_many :reservations
+  one_to_many :evaluations
+  one_to_many :results
 end
 
 class Aircrafts < Sequel::Model
 end
 
 class Attempts < Sequel::Model
+  many_to_one :evaluation
 end
 
 class Chapters < Sequel::Model
 end
 
 class Conditions < Sequel::Model
+  many_to_one :practical_exercise
 end
 
 class Courses < Sequel::Model
+  one_to_many :reservations
+  one_to_many :results
 end
 
 class Evaluations < Sequel::Model
+  one_to_many :attempts
+  many_to_one :practical_exercise
+  many_to_one :user
 end
 
 class Links < Sequel::Model
+  many_to_one :practical_exercise
 end
 
 class LogbookNotes < Sequel::Model
+  many_to_one :reservation
 end
 
 class PracticalExercises < Sequel::Model
+  one_to_many :evaluations
+  one_to_many :conditions
+  one_to_many :links
+  one_to_many :results
+  one_to_many :theory_links
 end
 
 class Reservations < Sequel::Model
+  many_to_one :course
+  many_to_one :user
 end
 
 class Results < Sequel::Model
+  many_to_one :practical_exercise
+  many_to_one :user
+  many_to_one :course
 end
 
 class TheoryLinks < Sequel::Model
-  
+  many_to_one :practical_exercise
 end
